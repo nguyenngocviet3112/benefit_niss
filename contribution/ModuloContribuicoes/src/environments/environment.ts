beb306053753f30803dba5042aac435b6ba34832
nguyenngocviet3112 <47234663+nguyenngocviet3112@users.noreply.github.com>
2025-06-16 08:17:46 +0900

    update code build fix contribute

--- contribution/ModuloContribuicoes/src/environments/environment.ts
+++ contribution/ModuloContribuicoes/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://api.testapp.segurancasocial.gov.tl/api',
+  // apiUrl: 'http://localhost:5000/api',
   wordpressUrl: '',
   ssIcon: 'assets/image/inss_logo.png',
   checkBoxIcon: 'assets/image/check_box.png'
